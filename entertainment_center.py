0623decfc14ef0ec2593d98f83c45bbac5c04b86
tracer11 <karolgurba22@gmail.com>
2018-02-12 14:44:05 -0600

    fixed issue where movie posters were not showing up

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,21 +3,23 @@ import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story", 
                         "A story of toys that come to life",
-                        "https://www.amazon.com/Toy-Story-Tim-Allen/dp/B0049J0DC8",
+                        "https://ohmy.disney.com/wp-content/uploads/2014/01/toy-story-poster.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 
 
 avatar = media.Movie("Avatar",
                       "A story of a alien civilization",
-                      "http://www.impawards.com/2009/avatar.html",
+                      "https://vignette.wikia.nocookie.net/avatar/images/3/3f/Avatar-poster.jpg/revision/latest?cb=20091209142005&path-prefix=hu",
                       "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 
 interstellar = media.Movie("Interstellar",
                             "A team goes to another galaxy to find a earth 2,0",
-                            "https://movieweb.com/interstellar-movie-imax-poster/",
+                            "http://www.joblo.com/posters/images/full/interstellar-mcconaughey.jpg",
                             "https://www.youtube.com/watch?v=0vxOhd4qlnA")
+
+
 
 movies = [toy_story, avatar, interstellar]
 fresh_tomatoes.open_movies_page(movies)
